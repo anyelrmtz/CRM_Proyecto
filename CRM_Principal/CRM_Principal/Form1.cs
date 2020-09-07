4603df5ecec95cca4d852314a04c90728c8d3d61
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-09-07 13:16:53 -0500

    prueba de conexion MySQL en la nube

--- CRM_Principal/CRM_Principal/Form1.cs
+++ CRM_Principal/CRM_Principal/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace CRM_Principal
 {
@@ -29,7 +30,23 @@ namespace CRM_Principal
 
         private void Ingresar_JD_Click(object sender, EventArgs e)
         {
-          
+            MySqlConnection conectar = new MySqlConnection("server=sql3.freemysqlhosting.net; database=sql3364286;Uid=sql3364286;pwd=j1AwNptgUn;");
+            conectar.Open();
+
+            MySqlCommand codigo = new MySqlCommand();
+            MySqlConnection conectanos = new MySqlConnection();
+            codigo.Connection = conectar;
+            codigo.CommandText = ("select * from usuarios where user = '"+txtuser.Text+"' and contra = '"+txtpass.Text+"'");
+            MySqlDataReader leer = codigo.ExecuteReader();
+            if (leer.Read())
+            {
+                MessageBox.Show("bienvenido");
+            }
+            else
+            {
+                MessageBox.Show("contraseña o usuario no existe");
+            }
+            conectar.Close();
         }
     }
 }
