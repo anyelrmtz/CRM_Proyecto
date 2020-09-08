f9620dd2ae5d6ac6e0de21025e1324daaa49db41
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-09-08 12:47:58 -0500

    entrada

--- CRM_Principal/CRM_Principal/Principal.Designer.cs
+++ CRM_Principal/CRM_Principal/Principal.Designer.cs
@@ -35,8 +35,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1011, 468);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Principal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Juno Doctor";
