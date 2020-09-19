12bc9a47e41529a9f5f211d879e62cc3a4652bdc
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-09-19 17:30:04 -0500

    actualizacion

--- CRM_Principal/CRM_Principal/Administracion/Cotizacion.Designer.cs
+++ CRM_Principal/CRM_Principal/Administracion/Cotizacion.Designer.cs
@@ -30,10 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cotizacion));
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comb_cotizar = new System.Windows.Forms.ComboBox();
+            this.combo_servicio = new System.Windows.Forms.ComboBox();
+            this.combo_tipo = new System.Windows.Forms.ComboBox();
+            this.cantidad_text = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -41,15 +41,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.label7 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -65,54 +65,47 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Cotización";
             // 
-            // comboBox1
+            // comb_cotizar
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Opciones",
+            this.comb_cotizar.FormattingEnabled = true;
+            this.comb_cotizar.Items.AddRange(new object[] {
             "Gastos",
             "Ingresos"});
-            this.comboBox1.Location = new System.Drawing.Point(150, 94);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
+            this.comb_cotizar.Location = new System.Drawing.Point(138, 95);
+            this.comb_cotizar.Name = "comb_cotizar";
+            this.comb_cotizar.Size = new System.Drawing.Size(136, 21);
+            this.comb_cotizar.TabIndex = 2;
+            this.comb_cotizar.Text = "Selecione una Opcion";
+            this.comb_cotizar.SelectedIndexChanged += new System.EventHandler(this.comb_cotizar_SelectedIndexChanged_1);
+            this.comb_cotizar.TabIndexChanged += new System.EventHandler(this.comb_cotizar_TabIndexChanged);
             // 
-            // comboBox2
+            // combo_servicio
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "Servicio Electrico",
-            "Servicio Potable",
-            "Renta",
-            "Internet"});
-            this.comboBox2.Location = new System.Drawing.Point(150, 180);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 3;
+            this.combo_servicio.FormattingEnabled = true;
+            this.combo_servicio.Location = new System.Drawing.Point(138, 181);
+            this.combo_servicio.Name = "combo_servicio";
+            this.combo_servicio.Size = new System.Drawing.Size(136, 21);
+            this.combo_servicio.TabIndex = 3;
+            this.combo_servicio.Text = "Selecione una Opcion";
+            this.combo_servicio.SelectedIndexChanged += new System.EventHandler(this.combo_servicio_SelectedIndexChanged);
             // 
-            // comboBox3
+            // combo_tipo
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Items.AddRange(new object[] {
-            "Opciones",
-            "Medicinas",
-            "Vacunas",
-            "Procesos",
-            "Consultas",
-            "Talleres",
-            "Cursos",
-            "Servicios"});
-            this.comboBox3.Location = new System.Drawing.Point(150, 135);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 21);
-            this.comboBox3.TabIndex = 4;
+            this.combo_tipo.FormattingEnabled = true;
+            this.combo_tipo.Location = new System.Drawing.Point(138, 136);
+            this.combo_tipo.Name = "combo_tipo";
+            this.combo_tipo.Size = new System.Drawing.Size(136, 21);
+            this.combo_tipo.TabIndex = 4;
+            this.combo_tipo.Text = "Selecione una Opcion";
+            this.combo_tipo.SelectedIndexChanged += new System.EventHandler(this.combo_tipo_SelectedIndexChanged);
+            this.combo_tipo.SelectedValueChanged += new System.EventHandler(this.combo_tipo_SelectedValueChanged);
             // 
-            // textBox1
+            // cantidad_text
             // 
-            this.textBox1.Location = new System.Drawing.Point(150, 221);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 5;
+            this.cantidad_text.Location = new System.Drawing.Point(138, 222);
+            this.cantidad_text.Name = "cantidad_text";
+            this.cantidad_text.Size = new System.Drawing.Size(121, 20);
+            this.cantidad_text.TabIndex = 5;
             // 
             // label2
             // 
@@ -162,6 +155,7 @@
             this.button1.TabIndex = 10;
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
@@ -184,55 +178,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(570, 363);
             this.panel1.TabIndex = 12;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(26, 50);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(67, 24);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Filtros";
-            // 
-            // comboBox4
-            // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Items.AddRange(new object[] {
-            "Opciones",
-            "Gastos",
-            "Ingresos"});
-            this.comboBox4.Location = new System.Drawing.Point(108, 50);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(121, 21);
-            this.comboBox4.TabIndex = 7;
-            // 
-            // comboBox5
-            // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Items.AddRange(new object[] {
-            "Opciones",
-            "Cursos",
-            "Talleres",
-            "Procedimietos",
-            "Servicios",
-            "Consultas",
-            "Vacunas",
-            "Medicinas"});
-            this.comboBox5.Location = new System.Drawing.Point(246, 50);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(121, 21);
-            this.comboBox5.TabIndex = 9;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(242, 14);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(100, 19);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "Informacion";
             // 
             // dataGridView1
             // 
@@ -267,6 +212,55 @@
             this.Column4.HeaderText = "Costo";
             this.Column4.Name = "Column4";
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(242, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 19);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Informacion";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Opciones",
+            "Cursos",
+            "Talleres",
+            "Procedimietos",
+            "Servicios",
+            "Consultas",
+            "Vacunas",
+            "Medicinas"});
+            this.comboBox5.Location = new System.Drawing.Point(246, 50);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(26, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Filtros";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Opciones",
+            "Gastos",
+            "Ingresos"});
+            this.comboBox4.Location = new System.Drawing.Point(108, 50);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 7;
+            // 
             // Cotizacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -278,10 +272,10 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cantidad_text);
+            this.Controls.Add(this.combo_tipo);
+            this.Controls.Add(this.combo_servicio);
+            this.Controls.Add(this.comb_cotizar);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -299,10 +293,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comb_cotizar;
+        private System.Windows.Forms.ComboBox combo_servicio;
+        private System.Windows.Forms.ComboBox combo_tipo;
+        private System.Windows.Forms.TextBox cantidad_text;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
--- CRM_Principal/CRM_Principal/Administracion/Cotizacion.cs
+++ CRM_Principal/CRM_Principal/Administracion/Cotizacion.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
+using System.Data.Sql;
 namespace CRM_Principal
 {
     public partial class Cotizacion : Form
@@ -15,6 +16,200 @@ namespace CRM_Principal
         public Cotizacion()
         {
             InitializeComponent();
+            Bloqueo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(comb_cotizar.Text== "Selecione una Opcion")
+            {
+                MessageBox.Show("Selecione una Cotizacion");
+            }
+            else
+            {
+                if(combo_tipo.Text== "Selecione una Opcion")
+                {
+                    MessageBox.Show("Selecione un tipo ");
+                }
+                else
+                {
+                    if(combo_servicio.Text== "Selecione una Opcion")
+                    {
+                        MessageBox.Show("Seleccione un Servicio");
+                    }
+                    else
+                    {
+                        if (cantidad_text.Text == "")
+                        {
+                            MessageBox.Show("ingrese un monto");
+                        }
+                        else
+                        {
+                            MessageBox.Show("agrgado exitosamente, este mensaje es de prueba no agrega datos a la BD");
+                        }
+                    }
+                }
+            }
+        }
+        //bloquea combobos asta llenar los campos
+        public void Bloqueo()
+        {
+            if (comb_cotizar.Text == "Selecione una Opcion")
+            {
+                combo_tipo.Enabled = false;
+                cantidad_text.Enabled = false;
+                
+
+            }
+        }
+
+        private void combo_tipo_SelectedValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comb_cotizar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comb_cotizar_TabIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        // verificamos que el combobox este con alguna opcion
+        private void comb_cotizar_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if(comb_cotizar.SelectedItem.ToString()== "Gastos")
+            {
+                Gastos();
+               
+            }
+            
+            else if (comb_cotizar.SelectedItem.ToString() == "Ingresos")
+            {
+
+                Ingresos();
+            }
+
+        }
+        public void Ingresos()
+        {
+            combo_servicio.Text = "Selecione una Opcion";
+            combo_tipo.Items.Clear();
+            combo_tipo.Text = "Selecione una Opcion";
+            combo_tipo.Items.Add("Procedimientos");
+
+            combo_servicio.Items.Clear();
+        }
+        public void Gastos()
+        {
+
+            combo_servicio.Text = "Selecione una Opcion";
+            combo_tipo.Text = "Selecione una Opcion";
+            combo_tipo.Items.Clear();
+            combo_tipo.Enabled = true;
+            combo_tipo.Items.Add("Vacunas");
+            combo_tipo.Items.Add("Servicios");
+
+        }
+
+
+        SqlConnection conectar = new SqlConnection("Data Source = 10.23.249.209; Initial Catalog = junodoctor; Persist Security Info = True; User ID = DOCTORJUNIO; Password = junodoctor2020");
+        //vacunas,procesos,Consultas,Talleres,Cursos,Servicios
+        public void Vacuna()
+        {
+            combo_servicio.Items.Clear();
+            conectar.Open();
+
+            SqlCommand medician = new SqlCommand();
+            SqlConnection conectanos = new SqlConnection();
+            medician.Connection = conectar;
+            medician.CommandText = ("select Nombre from Vacuna ");
+
+            SqlDataReader leer = medician.ExecuteReader();
+            while (leer.Read())
+            {
+                combo_servicio.Items.Add(leer["Nombre"].ToString());
+            }
+            
+            conectar.Close();
+        }
+
+        public void Procesos(){
+            combo_servicio.Items.Clear();
+            conectar.Open();
+            
+            SqlCommand proce = new SqlCommand();
+            SqlConnection conectanos = new SqlConnection();
+            proce.Connection = conectar;
+            proce.CommandText = ("select Nombre from Procedimiento ");
+
+            SqlDataReader leer = proce.ExecuteReader();
+            while (leer.Read())
+            {
+                combo_servicio.Items.Add(leer["Nombre"].ToString());
+            }
+
+            conectar.Close();
+        }
+       /* public void Taller()
+        {
+
+        }
+        public void Cursos()
+        {
+
+        }*/
+        public void Servicio()
+
+        {
+            combo_servicio.Items.Clear();
+            conectar.Open();
+
+            SqlCommand serv = new SqlCommand();
+            SqlConnection conectanos = new SqlConnection();
+            serv.Connection = conectar;
+            serv.CommandText = ("select Nombre from Servicios ");
+
+            SqlDataReader leer = serv.ExecuteReader();
+            while (leer.Read())
+            {
+                combo_servicio.Items.Add(leer["Nombre"].ToString());
+            }
+
+            conectar.Close();
+        }
+        //seleciona tipo de dato
+        private void combo_tipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (combo_tipo.SelectedItem.ToString() == "Vacunas")
+            {
+                
+                Vacuna();
+
+            }
+            else if (combo_tipo.SelectedItem.ToString() == "Procedimientos")
+            {
+                Procesos();
+            }
+            else if (combo_tipo.SelectedItem.ToString() == "Servicios")
+            {
+                Servicio();
+            }
+        }
+
+        private void combo_servicio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (combo_servicio.SelectedItem.ToString() == "")
+            {
+                cantidad_text.Enabled = false;
+            }
+            else
+            {
+                cantidad_text.Enabled = true;
+            }
         }
     }
 }
+
