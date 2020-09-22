c24e605e35bdf10e9cebb0bba92ddc34a4391080
Manuel Angel Rivera Martinez <anyel_rmtz26@hotmail.com>
2020-09-22 13:15:49 -0500

    actualizacion

--- CRM_Principal/CRM_Principal/Administracion/Cotizacion.Designer.cs
+++ CRM_Principal/CRM_Principal/Administracion/Cotizacion.Designer.cs
@@ -160,7 +160,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-8, -19);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(920, 487);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
